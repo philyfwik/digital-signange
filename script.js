ce6bedac06ead9b52c558be443838dcfda080aea
philipo <philyfwik@gmail.com>
2023-03-20 16:26:31 -0600

    feat: only show overlays when videos are >= 2 seconds

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const signage = document.getElementById('signage-container');
 const mainExp = document.getElementById('mainExp');
 const leftVideo = document.getElementById('left-video');
+const leftOverlay = document.getElementById('left-overlay');
 const rightVideo = document.getElementById('right-video');
+const rightOverlay = document.getElementById('right-overlay');
 const takeOver = document.getElementById('takeOver');
 
 let showingMainExp = true;
@@ -12,6 +14,7 @@ function showTakeOver() {
   mainExp.style.opacity = 0;
   leftVideo.pause();
   rightVideo.pause();
+  clearInterval(displayTime);  // stop tracking currentTime of right and left displays
   showingMainExp = false;
   console.log('Displaying TakeOver Video');
   switchDisplay();
@@ -22,9 +25,25 @@ function showMainExperience() {
   mainExp.style.opacity = 1;
   leftVideo.play();
   rightVideo.play();
+  showOverLay(2, 7.5);
   showingMainExp = true;
   console.log('Displaying Main Experience');
   switchDisplay();
+}
+
+// Show Overlay when left and right videos are within the minTime and maxTime window
+let displayTime;  // to track playback time of displays every second
+function showOverLay(minTime, maxTime) {
+  displayTime = setInterval(() => {
+    let time = rightVideo.currentTime;
+    if (time < minTime || time > maxTime) {
+      leftOverlay.style.opacity = 0;
+      rightOverlay.style.opacity = 0;
+    } else {
+      leftOverlay.style.opacity = 1;
+      rightOverlay.style.opacity = 1;
+    }
+  }, 1000);
 }
 
 // Toggle Displays
@@ -37,3 +56,4 @@ function switchDisplay() {
 }
 
 switchDisplay();
+showOverLay(2, 7.5);
